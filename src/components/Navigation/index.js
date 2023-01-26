c38bdfcfc47f598aa9efad02e71a3989b8aba513
Kristian Andersen <hello@kristian.co>
2023-01-26 12:54:21 +0100

    Fixed link import

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
-import Link from 'next/Link';
+import Link from 'next/link';
 
 import ctActive from "./ct-active.png";
 import ctInactive from "./ct-inactive.png";
